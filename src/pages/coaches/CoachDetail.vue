8d32b996cfb07df1c7b6d6eae032b7f25cb6da4f
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-03-29 19:13:44 -0400

    Adds initial components and routes

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,5 @@
+<template>
+    Coach Details
+    <router-view></router-view>
+    <router-link to="/coaches/1/contact">Contact</router-link>
+</template>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>Filter</section>
+    <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register"> Register as Coach</router-link>
+    </div>
+    <section>List of Coaches</section>
+</template>
+
+<script>
+
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+
+<style scoped>
+
+</style>

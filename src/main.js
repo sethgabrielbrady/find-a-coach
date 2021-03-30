f4515e51e7cdec34dfd6a265f109376fe713d6da
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-03-29 22:02:46 -0400

    Updates Vuex and some state management.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+
 import { createApp } from 'vue';
+
 import router from './router.js';
+import store from './stores/index.js';
 import App from './App.vue';
 
 const app = createApp(App)
+
 app.use(router);
+app.use(store);
+
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,19 +4,48 @@
         <button>Refresh</button>
         <router-link to="/register"> Register as Coach</router-link>
     </div>
-    <section>List of Coaches</section>
+    <ul v-if="hasCoaches">
+        <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :area="coach.areas"
+        ></coach-item>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 
 export default {
-    setup() {
-        
+    components: {
+        CoachItem
     },
+    computed: {
+        filteredCoaches() {
+            return this.$store.getters['coaches/coaches'];
+        },
+        hasCoaches() {
+            return this.$store.getters['coaches/hasCoaches'];
+        },
+    }
 }
 </script>
 
 
 <style scoped>
+    ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    }
 
+    .controls {
+    display: flex;
+    justify-content: space-between;
+    }
 </style>
